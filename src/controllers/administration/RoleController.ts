03eb0486dab11189fd80c9a6f7b9930bc611ff8d
leo-mutuku <le.mutuku@gmail.com>
2024-11-16 08:34:50 +0300

    update administration controller presentation layer , actual implimentation is yet

---
+++ src/controllers/administration/RoleController.ts
@@ -0,0 +1,12 @@
+import { NextFunction, Request, Response } from "express";
+
+export class RoleController {
+    async onCreateRole(req: Request, res: Response, next: NextFunction) {
+    }
+    async onGetRoles(req: Request, res: Response, next: NextFunction) {
+    }
+    async onGetRole(req: Request, res: Response, next: NextFunction) {
+    }
+    async onUpdateRole(req: Request, res: Response, next: NextFunction) {
+    }
+}
